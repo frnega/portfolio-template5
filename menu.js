34ce4fbf67500ff42a5e9bfd63ce4fdcdc4d08b5
frehiywot nega <fr.nega@gmail.com>
2022-10-11 13:00:15 +0300

    fix to eslint

--- menu.js
+++ menu.js
@@ -1,20 +1,19 @@
 const lastWidth = document.querySelector('body').style.width;
-const lastHeight = document.querySelector('body').style.height;
 
 const closeMenu = function () {
   document.getElementsByClassName('menu')[0].classList.add('hidden');
   document.querySelector('body').style.width = lastWidth;
-  document.getElementsByClassName('headline')[0].classList.remove('hidden'); 
+  document.getElementsByClassName('headline')[0].classList.remove('hidden');
   document.getElementsByClassName('Works')[0].classList.remove('hidden');
-  document.getElementsByClassName('About')[0].classList.remove('hidden'); 
-  document.getElementsByClassName('form')[0].classList.remove('hidden'); 
+  document.getElementsByClassName('About')[0].classList.remove('hidden');
+  document.getElementsByClassName('form')[0].classList.remove('hidden');
 };
 
 const openMenu = function () {
   document.getElementsByClassName('menu')[0].classList.remove('hidden');
-  document.querySelector('body').style.width = '10vmax'; 
-  document.getElementsByClassName('headline')[0].classList.add('hidden'); 
-  document.getElementsByClassName('Works')[0].classList.add('hidden'); 
+  document.querySelector('body').style.width = '10vmax';
+  document.getElementsByClassName('headline')[0].classList.add('hidden');
+  document.getElementsByClassName('Works')[0].classList.add('hidden');
   document.getElementsByClassName('About')[0].classList.add('hidden');
   document.getElementsByClassName('form')[0].classList.add('hidden');
 };
